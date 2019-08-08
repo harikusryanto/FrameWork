d837e7678e21d13261f1aa9232114511d72eb9f4
Hari Kusryanto <hari17ti@mahasiswa.pcr.ac.id>
2019-08-09 05:03:55 +0700

    Update contact.php

--- application/views/contact.php
+++ application/views/contact.php
@@ -103,18 +103,17 @@
                         <div class="contact_info">
                             <div class="info_item">
                                 <i class="lnr lnr-home"></i>
-                                <h6>California, United States</h6>
-                                <p>Santa monica bullevard</p>
+                                <h6>Politeknik Caltex Riau</h6>
+                                <p>Jln.UmbanSari</p>
                             </div>
                             <div class="info_item">
                                 <i class="lnr lnr-phone-handset"></i>
                                 <h6><a href="#">00 (440) 9865 562</a></h6>
-                                <p>Mon to Fri 9am to 6 pm</p>
+                                
                             </div>
                             <div class="info_item">
                                 <i class="lnr lnr-envelope"></i>
-                                <h6><a href="#">support@colorlib.com</a></h6>
-                                <p>Send us your query anytime!</p>
+                                <h6><a href="#">17tib@mahasiswa.pcr.ac.id</a></h6>
                             </div>
                         </div>
                     </div>
